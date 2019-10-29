78654f8925a0132db599cbf2d94349ed07aa6f33
JennAnnDag7 <jenniferd7@hotmail.com>
2019-10-29 19:14:17 -0400

    finish #1

---
+++ min_max.py
@@ -0,0 +1,23 @@
+import math
+
+the_array = [15, 22, 84, 14, 88, 23]
+the_highest_value = 88
+the_lowest_value = 14
+
+def min_max():
+    for num in the_array:
+        print(max)
+    for num in the_array:
+        print(min)
+    return max
+
+    # if the_array contains the_highest_value:
+    #     print(the_highest_value)
+    # if the_array contains the_smallest_number:
+    #     print(the_smallest_number)
+    # return 
+    
+    
+print(the_highest_value-the_lowest_value)
+    
+
